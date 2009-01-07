e618d5969adabd8ca18fbc3e12b3546dc6872a45
Jason Morrison <jasonmorrison@jason-morrisons-macbook.local>
2009-01-06 19:41:43 -0500

    added test markup to test/rails_root app

--- test/rails_root/test/functional/home_controller_test.rb
+++ test/rails_root/test/functional/home_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index_action
+    get :index
+    assert_response :success
+    assert_template 'index'
+  end
+
+  def test_markup_on_index
+    get :index
+    assert_select 'div.milestone1'
+    assert_select 'div.milestone2'
+  end
+
+  def test_javascript_includes
+    get :index
+    %w(jquery jquery-ui jrails).each do |jslib|
+      assert_select 'script[src=?]', /.*#{jslib}.*/
+    end
   end
 end
